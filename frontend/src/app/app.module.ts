17b6244434b7ea345e28ab1b80ea5c82640db27f
Délita Makanda <delita.makanda@gmail.com>
2019-03-24 01:58:28 +0100

    install translations

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,23 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { HttpClientModule } from '@angular/common/http';
 
 
 import { MaterialModule } from './material.module';
+import { TranslateService } from './services/translate/translate.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { TranslatePipe } from './pipes/translate/translate.pipe';
+
+export function setupTranslateFactory (service: TranslateService): Function {
+    return () => service.use('fr');
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    TranslatePipe
   ],
   imports: [
     BrowserModule,
@@ -28,7 +35,15 @@ import { LoginComponent } from './login/login.component';
     ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'}),
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+      TranslateService,
+      {
+          provide: APP_INITIALIZER,
+          useFactory: setupTranslateFactory,
+          deps: [TranslateService],
+          multi: true
+      }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
