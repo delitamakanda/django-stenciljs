a5bcf23dc097952ef783329be83105dd3b8fee87
Délita Makanda <delita.makanda@gmail.com>
2019-05-04 16:38:57 +0200

    add smtp email

--- dating/settings_production.py
+++ dating/settings_production.py
@@ -12,3 +12,14 @@ DEBUG = False
 ALLOWED_HOSTS = ['*',]
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# Mail
+SERVER_EMAIL = 'no-reply@lov3r.com'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('SENDGRID_SERVER')
+EMAIL_PORT = os.environ.get('SENDGRID_PORT')
+EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = SERVER_EMAIL
