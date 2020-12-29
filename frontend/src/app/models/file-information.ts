3306c7f3362b1810b5c5e5093c2324b4d0b076b7
Délita Makanda <delita.makanda@gmail.com>
2020-12-29 19:37:13 +0100

    remove frontend folder

--- frontend/src/app/models/file-information.ts
+++
@@ -1,4 +0,0 @@
-export interface FileInformation {
-    filename: string;
-    size: number;
-}
--- frontend/src/app/models/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    id: number;
-    username: string;
-    displayName: string;
-    uri: string;
-    token?: string;
-    expires?: string;
-    constructor() {}
-}
